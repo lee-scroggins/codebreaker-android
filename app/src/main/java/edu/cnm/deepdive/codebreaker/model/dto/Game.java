fc6d9889c26e94250f40c2a921b117c09aa92a45
Lee Scroggins <lscrggns@yahoo.com>
2021-07-12 15:05:35 -0600

    Starting Room persistence classes.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/model/dto/Game.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/model/dto/Game.java
@@ -1,20 +1,13 @@
-package edu.cnm.deepdive.codebreaker.model;
+package edu.cnm.deepdive.codebreaker.model.dto;
 
-import androidx.room.ColumnInfo;
-import androidx.room.Entity;
-import androidx.room.Ignore;
-import androidx.room.PrimaryKey;
 import com.google.gson.annotations.Expose;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
-@Entity
 public class Game {
 
   @Expose
-  @PrimaryKey
-  @ColumnInfo(name = "game_id")
   private String id;
 
   @Expose
@@ -27,13 +20,11 @@ public class Game {
   private int length;
 
   @Expose
-  @ColumnInfo(name = "guess_count")
   private int guessCount;
 
   @Expose
   private boolean solved;
 
-  @Ignore
   private final List<Guess> guesses = new LinkedList<>();
 
 
